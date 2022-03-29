ed1db446f9e9d8d10f756818fe8a60a84acae6bf
stase <stasenko-andrei2002@mail.ru>
2022-03-29 12:26:02 +0300

    creating elements via a separate function/ added contentEditable

--- JS.js
+++ JS.js
@@ -1,72 +1,54 @@
-document.querySelector('#add-to-do__text-task').addEventListener('keydown',function (e){
-    if (e.keyCode === 13){
-        fun_add_to_do();
+const enterKey = document.querySelector('#add-to-do__text-task').addEventListener('keydown',function (e){
+    if (e.key === 'Enter'){
+        addToDo();
     }
 })
-document.querySelector('#add-to-do__button-add').onclick = function(){
-    fun_add_to_do();
-};
-function fun_add_to_do(){
-    let text_task = document.querySelector('#add-to-do__text-task').value;
+document.querySelector('#add-to-do__button-add').addEventListener('click', function(){
+    addToDo();
+});
 
-    if(text_task.trim() !== ''){
-        const list_task = document.querySelector('.tasks__list-tasks');
+function createElement(elName,elClasses = [],text='',typeEvent = [],event = [],type = '',contentEdit = false) {
+    const el = document.createElement(elName);
+    if (elClasses.length){
+        elClasses.forEach(className => el.classList.add(className));
+    }
+    if (text){
+        el.textContent = text;
+    }
+    if (event.length){
+        event.forEach(event => el.addEventListener(typeEvent, ev => event(ev.target)));
+    }
+    if (type){
+        el.type = type;
+    }
+    if(contentEdit){
+        el.contentEditable = true;
+    }
+    return el;
+}
 
-        const new_task = document.createElement('li');
-        new_task.className="list-tasks__style";
+function addToDo(){
+    let textTask = document.querySelector('#add-to-do__text-task').value;
 
-        const checkbox = document.createElement('input');
-        checkbox.type="checkbox";
-        checkbox.className="list-tasks__item_checkbox";
-        checkbox.addEventListener('click',ev => fun_done_task(ev.target));
-
-        const delete_task = document.createElement('button');
-        delete_task.innerText="X";
-        delete_task.className="list-tasks__item_delete-task";
-        delete_task.addEventListener('click',ev => fun_delete_task(ev.target));
-
-        const task = document.createElement('label');
-        task.className="list-tasks__item_text-task";
-        task.textContent=text_task.trim();
-        task.addEventListener('dblclick', ev => fun_change_task(ev.target));
-
-        new_task.appendChild(checkbox);
-        new_task.appendChild(delete_task);
-        new_task.appendChild(task);
-
-        list_task.appendChild(new_task);
+    if(textTask.trim()){
+        const newListTask = document.querySelector('.tasks__list-tasks');
+        const newTask = createElement('li',['list-tasks__style'])
+        const checkboxT = createElement('input',['list-tasks__item_checkbox'],'',['click'],[doneTaskStyle],'checkbox')
+        const deleteT = createElement('button',['list-tasks__item_delete-task'],'X',['click'],[deleteTask])
+        const textT = createElement('label',['list-tasks__item_text-task'],textTask.trim(),'','','',true)
+        newTask.appendChild(checkboxT);
+        newTask.appendChild(deleteT);
+        newTask.appendChild(textT);
+        newListTask.appendChild(newTask);
 
         document.querySelector('#add-to-do__text-task').value = '';
     }
-
-}
-function fun_change_task(ev){
-    const input = document.createElement('input');
-    input.className = "list-tasks__item_text-task"
-    input.id ="input_change"
-    input.addEventListener('focusout',ev => fun_change_task_done(ev.target));
-    input.addEventListener('keydown',function (e){
-        if (e.keyCode === 13){
-            fun_change_task_done(e.target);
-        }
-    })
-    input.value = ev.textContent;
-    ev.replaceWith(input);
-    document.querySelector('#input_change').focus();
 }
 
-function fun_change_task_done(ev) {
-    const task = document.createElement('label');
-    task.className="list-tasks__item_text-task";
-    task.textContent=ev.value;
-    task.addEventListener('dblclick', ev => fun_change_task(ev.target));
-    ev.replaceWith(task);
-}
-
-function fun_delete_task(ev){
+function deleteTask(ev){
     ev.parentNode.remove();
 }
 
-function fun_done_task(ev){
+function doneTaskStyle(ev){
     ev.parentNode.className = ev.checked ? 'list-tasks__style list-tasks__style_done':'list-tasks__style';
 }
